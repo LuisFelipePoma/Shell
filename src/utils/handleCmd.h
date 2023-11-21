f6d561f5fa24f3900b68c2d9bc8477343445b0f5
LuisFelipePoma <u202110902@upc.edu.pe>
2023-11-21 18:37:29 -0500

    fix handle

--- src/utils/handleCmd.h
+++ src/utils/handleCmd.h
@@ -11,8 +11,6 @@
 #include <memory>
 #include <fstream>
 
-std::string execAndCapture(const std::string &command);
-
 inline std::string handleExecutionCmd(const std::string &command, const bool isPipeline)
 {
 	std::string result = "";
@@ -29,52 +27,9 @@ inline std::string handleExecutionCmd(const std::string &command, const bool isP
 	}
 	else
 	{
-		result = execAndCapture(command.c_str());
-		std::cout << "\033[1;32m" << result << "\033[0m\n";
+		std::system(command.c_str());
 	}
 	return result;
 }
 
-inline std::string execAndCapture(const std::string &command)
-{
-	std::array<char, 128> buffer;
-	std::string result;
-	std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"), pclose);
-	if (!pipe)
-	{
-		std::cout << "popen() failed!";
-		return "error";
-	}
-	while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr)
-	{
-		result += buffer.data();
-	}
-	return result;
-}
-
-inline void passStringToCommand(const std::string &str, const std::string &command)
-{
-	// Write the string to a temporary file
-	std::ofstream tempFile("temp.txt");
-	tempFile << str;
-	tempFile.close();
-
-	// Construct the command
-	std::string fullCommand = command + " temp.txt";
-
-	// Execute the command
-	std::system(fullCommand.c_str());
-
-	// Delete the temporary file
-	std::remove("temp.txt");
-}
-
-inline void execAndRedirect(const std::string &command, const std::string &outputFile)
-{
-	// Construct the command
-	std::string fullCommand = command + " > " + outputFile;
-
-	// Execute the command
-	std::system(fullCommand.c_str());
-}
 #endif
